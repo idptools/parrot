9bdca629609de6b31d9a0bcebaf21db74e0b22ef
Dan Griffith <degriffith@wustl.edu>
2022-01-16 12:29:53 -0600

    Update setup.py package version dependencies

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # url='http://www.my_package.com',  # Website
     # download_url = 'https://github.com/idptools/parrot/archive/v0.1-alpha.tar.gz',
     install_requires=[
-            'torch',
+            'torch>=1.8.0,<=1.10.1',
             'numpy',
             'argparse',
             'more-itertools',
@@ -71,7 +71,7 @@ setup(
     #            'Mac OS-X',
     #            'Unix',
     #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    python_requires=">=3.5",          # Python version restrictions
+    python_requires=">=3.7,<=3.8.11",          # Python version restrictions
 
     # Manual control if final package is compressible or not, set False to prevent the .egg from being made
     zip_safe=False,
